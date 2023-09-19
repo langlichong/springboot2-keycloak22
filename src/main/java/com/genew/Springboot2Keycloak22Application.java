741e6c627ab7fac1eaafa00ffee901f6f8ba1c92
zhangwei <zhangwei3@genew.com>
2023-09-19 10:58:39 +0800

    feat: 添加SSLTool工具，信任所有证书，忽略对开启了Https kecloak的证书验证

--- src/main/java/com/genew/Springboot2Keycloak22Application.java
+++ src/main/java/com/genew/Springboot2Keycloak22Application.java
@@ -1,5 +1,6 @@
 package com.genew;
 
+import com.genew.util.SSLTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class Springboot2Keycloak22Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2Keycloak22Application.class, args);
+		SSLTool.disableCertificateValidation();
 	}
 
 }
